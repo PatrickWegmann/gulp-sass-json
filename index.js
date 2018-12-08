53b71400a49a4e2af117fc1a47190b53f25baeb6
PatrickWegmann <32100939+PatrickWegmann@users.noreply.github.com>
2018-12-08 11:22:33 +0100

    Clean up

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-var through = require('through2');    // npm install --save through2
+var through = require('through2');  
 
 module.exports = function () {
     return through.obj(function (file, encoding, callback) {
 
         var context = this;
         var jsonVariables = {};
-
-        // parse variable from scss/sass
 
         var lineReader = require('readline').createInterface({
             input: require('fs').createReadStream(file.path)
@@ -16,15 +14,15 @@ module.exports = function () {
 
             var regex = /^\$(.*)\: +(.*);/g;
             var match = regex.exec(line);
-            if (match !== null) {
+            if (match !== null && match.length === 3) {
 
                 var field = match[1];
                 var val = match[2];
                 jsonVariables[field] = val;
-                console.log('YES Line from file:', line);
             }
-            else {
-                console.log('Line from file:', line);
+            else
+            {
+                console.log("Error parsing line: " + line);
             }
         });
 
@@ -34,8 +32,7 @@ module.exports = function () {
             file.contents = Buffer.from(JSON.stringify(jsonVariables, null, 2));
             file.path = file.path.replace(".scss", ".json").replace(".sass", ".json");
             context.push(file);
-            callback();
-            
+            callback();           
         });
     });
 };
